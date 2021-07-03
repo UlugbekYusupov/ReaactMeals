290941841ef5e21257cd77dd0de4c42a01ca0ef9
Bob Pakhriev <boburpakhriev@gmail.com>
2021-07-03 12:00:36 +0900

    Refactor code

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { AuthContextProvider } from './store/auth-context';
-import CardProvider from './store/CardProvider';
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "./store/auth-context";
+import CardProvider from "./store/CardProvider";
+
+const root = document.getElementById("root");
 
 ReactDOM.render(
-    <AuthContextProvider>
-        <CardProvider>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </CardProvider>
-    </AuthContextProvider>
-    ,
-    document.getElementById('root')
+  <AuthContextProvider>
+    <CardProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </CardProvider>
+  </AuthContextProvider>,
+  root
 );
